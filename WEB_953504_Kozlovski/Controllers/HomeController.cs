46c741d03e1a13f81d85fec7edb34a66daef6a62
Artem Kozlovski <61842660+temakazik@users.noreply.github.com>
2021-10-04 23:40:50 +0300

    Added view and partial components

--- WEB_953504_Kozlovski/Controllers/HomeController.cs
+++ WEB_953504_Kozlovski/Controllers/HomeController.cs
@@ -1,37 +1,103 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using WEB_953504_Kozlovski.Models;
 
 namespace WEB_953504_Kozlovski.Controllers
 {
+    public class ListDemo
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
+    }
+
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private List<ListDemo> _listDemo = new List<ListDemo>
         {
-            _logger = logger;
+            new ListDemo { ListItemValue = 1, ListItemText = "Item 1" },
+            new ListDemo { ListItemValue = 2, ListItemText = "Item 2" },
+            new ListDemo { ListItemValue = 3, ListItemText = "Item 3" }
+        };
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Lst"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
+            ViewData["Text"] = "Лабораторная работа 2";
+            return View();
         }
 
-        public IActionResult Index()
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        // GET: Home/Create
+        public ActionResult Create()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        // POST: Home/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
--- WEB_953504_Kozlovski/Startup.cs
+++ WEB_953504_Kozlovski/Startup.cs
@@ -35,6 +35,7 @@ namespace WEB_953504_Kozlovski
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
